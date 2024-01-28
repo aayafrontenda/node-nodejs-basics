5c1b65043816c3bd978c3bbe1468a93b928c3852
aayafrontenda <eeaayeyeah@gmail.com>
2024-01-28 22:27:16 +0600

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    if (fs.existsSync(path.normalize("./files/fresh.txt"))) {
+      throw new Error("FS operation failed");
+    }
+    fs.appendFile(
+      path.normalize("./files/fresh.txt"),
+      "I am fresh and young",
+      function (error) {
+        if (error) {
+          throw new Error("FS (write) operation failed");
+        }
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await create();
