7f46f1e37ad7cb1343c0a128b7032efda4c479e3
aayafrontenda <eeaayeyeah@gmail.com>
2024-01-28 23:30:30 +0600

    feat: implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { promises } from "fs";
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await promises.access(path.normalize("./files"));
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await promises.access(path.normalize("./files-copy"));
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await promises.cp(
+        path.normalize("./files"),
+        path.normalize("./files-copy"),
+        { recursive: true }
+      );
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 await copy();
