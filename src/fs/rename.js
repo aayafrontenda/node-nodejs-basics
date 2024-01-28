464d3a1b32cc563b5e28d4be2eac07982d2e0b7e
aayafrontenda <eeaayeyeah@gmail.com>
2024-01-28 23:39:27 +0600

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import { promises } from "fs";
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await promises.access(path.normalize("./files/wrongFilename.txt"));
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await promises.access(path.normalize("./files/properFilename.txt"));
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await promises.rename(
+        path.normalize("./files/wrongFilename.txt"),
+        path.normalize("./files/properFilename.txt")
+      );
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 await rename();
