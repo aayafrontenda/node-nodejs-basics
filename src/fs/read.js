7ee4ad024a744c1db8b5e4631db09fe123bfd052
aayafrontenda <eeaayeyeah@gmail.com>
2024-01-29 21:18:17 +0600

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { promises } from "fs";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    console.log(
+      await promises.readFile(path.normalize("./files/fileToRead.txt"), "utf-8")
+    );
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.error("FS operation failed");
+    }
+  }
 };
 
 await read();
