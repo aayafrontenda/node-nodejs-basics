1a65d74af4ae1fed63d96e354b567f00f1ca4b80
aayafrontenda <eeaayeyeah@gmail.com>
2024-01-30 02:58:04 +0600

    feat: implement zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,8 @@
+import zlib from "zlib";
+import fs from "fs";
 const compress = async () => {
-    // Write your code here 
+  const rs = fs.createReadStream("./files/fileToCompress.txt");
+  rs.pipe(zlib.createGzip()).pipe(fs.createWriteStream("./files/archive.gz"));
 };
 
 await compress();
