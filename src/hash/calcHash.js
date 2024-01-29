a1459065036f1426aa5d6c1b09dd9ffd3e640539
aayafrontenda <eeaayeyeah@gmail.com>
2024-01-29 23:09:27 +0600

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const rs = fs.createReadStream("./files/fileToCalculateHashFor.txt");
+  let content = "";
+  rs.on("data", (chunk) => {
+    content += chunk;
+  });
+  rs.on("end", () => {
+    const hash = crypto
+      .createHash("sha256")
+      .update(content, "utf-8")
+      .digest("hex");
+    console.log(hash);
+  });
 };
 
 await calculateHash();
